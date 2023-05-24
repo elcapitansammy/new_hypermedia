ec77f663b1a3cd1ed148aaa4b710bf5d8aef5349
Samuele Gentili <samuele.gentili@student.kuleuven.be>
2023-05-24 14:31:58 +0200

    database implemented for projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -3,7 +3,7 @@
       <h1>All projects</h1>
 
       <v-row>
-          <v-col cols="3" sm="1" md="4" v-for="(p, index) in template" :key="index" >
+          <v-col cols="3" sm="1" md="4" v-for="(p, index) in data" :key="index" >
               <Project_card :project="p"/>
           </v-col>
       </v-row>
@@ -11,14 +11,8 @@
 </template>
 
 <script setup>
-    const template = [
-        {"title": "Project1", "description": "fdsjakf"},
-        {"title": "Project2", "description": "fdsjakf"},
-        {"title": "Project3", "description": "fdsjakf"},
-        {"title": "Project4", "description": "fdsjakf"},
-        {"title": "Project5", "description": "fdsjakf"},
-        {"title": "Project6", "description": "fdsjakf"},
-    ]
+    let data = await $fetch('/server/getprojects'); 
+    console.log(data); 
 </script>
 
 <style scoped></style>
