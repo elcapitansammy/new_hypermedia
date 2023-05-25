7a2560991ff85033b8c0c933beaf78555e01833f
arinash <arina.shumeljuk@gmail.com>
2023-05-25 15:27:31 +0200

    Added Id pages

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,9 +1,17 @@
 <template>
   <div>
-    <p>Project id: {{ id }}</p>
+    <!-- <p>Project title: {{ project.name }}</p> -->
+    <p>We are here</p>
   </div>
 </template>
 
 <script setup>
   const {id} = useRoute().params
+
+  const uri = 'http://localhost:3001/server/getprojects/' + id
+
+  const { data: project} = await useFetch(uri)
+
+  console.log(project)
+
 </script>
